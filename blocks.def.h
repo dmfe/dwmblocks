a705861eb99505539bb15535f2ab992a177a5104
Dmitry Fedotov <dm.fe@yandex.ru>
2021-09-02 22:02:34 +0300

    Add battery status block.

--- blocks.def.h
+++ blocks.def.h
@@ -2,8 +2,9 @@
 static const Block blocks[] = {
     /*Icon*/    /*Command*/     /*Interval*/    /*Signal*/
     {"",        "memory",       30,             10},
-    {"",        "volume",       0,              11},
-    {"",        "datetime",     60,             12},
+    {"",        "battery",      30,             11},
+    {"",        "volume",       0,              12},
+    {"",        "datetime",     60,             13},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
